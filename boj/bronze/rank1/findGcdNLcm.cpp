02c6c304ce0c9c8a378955064f171fcff25d6050
Clarus23 <dnjs3023@gmail.com>
2024-05-29 19:09:50 +0900

    최대공약수와 최소공배수

---
+++ boj/bronze/rank1/findGcdNLcm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	int n1=0, n2=0;
+	cin >> n1 >> n2;
+	
+	int min = (n1 < n2) ? n1 : n2;
+	int max = (n1 > n2) ? n1 : n2;
+	while(min) {
+		int temp = max%min;
+		max = min;
+		min = temp;
+	}
+	
+	cout << max << endl << (n1/max)*n2;
+	
+	return 0;
+}
