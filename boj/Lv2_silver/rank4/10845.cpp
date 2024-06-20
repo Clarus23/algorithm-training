77a86c393d83cf11673bde3c9d508383a3783e21
Clarus23 <dnjs3023@gmail.com>
2024-06-20 15:21:09 +0900

    큐

---
+++ boj/Lv2_silver/rank4/10845.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    int *queue = new int[n+1];
+    int front = 0, rear = 0;
+    string cmd; int element;
+    for(int i=0; i<n; i++) {
+        cin >> cmd;
+        if(cmd == "push") {
+            scanf("%d", &element);
+            rear = (rear+1)%(n+1);
+            queue[rear] = element;
+        } else if(cmd == "pop") {
+            if(front == rear) printf("-1\n");
+            else {
+                front = (front+1)%(n+1);
+                printf("%d\n", queue[front]);
+            }
+        } else if(cmd == "front") {
+            if(front == rear) printf("-1\n");
+            else printf("%d\n", queue[front+1]);
+        } else if(cmd == "back") {
+            if(front == rear) printf("-1\n");
+            else printf("%d\n", queue[rear]);
+        }
+        else if(cmd == "empty") printf("%d\n", (front == rear));
+        else if(cmd == "size") printf("%d\n", (n+1+rear-front)%(n+1));
+    }
+
+    return 0;
+}
