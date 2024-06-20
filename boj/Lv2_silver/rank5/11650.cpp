b5e9f154a3617c9ad9ebd2ba33d831826c4afe3c
Clarus23 <dnjs3023@gmail.com>
2024-06-20 16:32:27 +0900

    좌표 정렬하기

---
+++ boj/Lv2_silver/rank5/11650.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    int n; cin >> n;
+    pair<int, int>* points = new pair<int, int>[n];
+    for(int i=0; i<n; i++) cin >> points[i].first >> points[i].second;
+
+    sort(points, points+n, [](pair<int, int>& a, pair<int, int>& b) {
+        return (a.first < b.first || (a.first == b.first && a.second < b.second));
+    });
+
+    for(int i=0; i<n; i++) cout << points[i].first << ' ' << points[i].second << '\n';
+
+    return 0;
+}
