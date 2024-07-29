495be0ec4698f48e0a6614dd165ae654ee47721d
Clarus23 <dnjs3023@gmail.com>
2024-07-29 21:48:31 +0900

    피보나치 수

---
+++ boj/Lv1_bronze/rank2/2747.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n; scanf("%d", &n);
+    int* fibo = new int[n];
+    fibo[0] = 1; fibo[1] = 1;
+    for(int i=2; i<n; i++) fibo[i] = fibo[i-1] + fibo[i-2];
+
+    printf("%d", fibo[n-1]);
+    
+    delete []fibo;
+    return 0;
+}
