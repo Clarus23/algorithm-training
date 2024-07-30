ebbf924421749e17f898d20102d9cd12eb241ee5
Clarus23 <dnjs3023@gmail.com>
2024-07-30 13:10:27 +0900

    최대 힙

---
+++ boj/Lv2_silver/rank2/11279.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void) {
+    priority_queue<int, vector<int>> pq;
+    int n; scanf("%d", &n);
+    while(n--) {
+        int x; scanf("%d", &x);
+
+        if(x) { pq.push(x); continue; }
+        if(pq.empty()) { printf("0\n"); continue; }
+
+        printf("%d\n", pq.top()); pq.pop();
+    }
+    return 0;
+}
