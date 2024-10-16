ad8e4fcbf0374ab319726ef0ca4bd2386f7fe1b5
Clarus23 <dnjs3023@gmail.com>
2024-10-16 18:37:36 +0900

    RGB거리

--- boj/Lv2_silver/rank1/1149.cpp
+++ boj/Lv2_silver/rank1/1149.cpp
@@ -6,16 +6,15 @@ using namespace std;
 
 int main() {
     int n; scanf("%d", &n);
-    vector<tuple<int, int, int>> cost(n);
-    for(tuple<int, int, int>& rgb : cost) scanf("%d %d %d", &get<0>(rgb), &get<1>(rgb), &get<2>(rgb));
-
     vector<tuple<int, int, int>> dp(n);
-    dp[0] = cost[0];
+    scanf("%d %d %d", &get<0>(dp[0]), &get<1>(dp[0]), &get<2>(dp[0]));
 
     for(int i=1; i<n; i++) {
-        int r = get<0>(cost[i]) + min(get<1>(dp[i-1]), get<2>(dp[i-1]));
-        int g = get<1>(cost[i]) + min(get<0>(dp[i-1]), get<2>(dp[i-1]));
-        int b = get<2>(cost[i]) + min(get<0>(dp[i-1]), get<1>(dp[i-1]));
+        int r, g, b; scanf("%d %d %d", &r, &g, &b);
+
+        r += min(get<1>(dp[i-1]), get<2>(dp[i-1]));
+        g += min(get<0>(dp[i-1]), get<2>(dp[i-1]));
+        b += min(get<0>(dp[i-1]), get<1>(dp[i-1]));
 
         dp[i] = {r, g, b};
     }
